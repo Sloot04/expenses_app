8c0ef2f8033d9c75a177130f8845740190dff660
Sloot04 <suyaislootmans@gmail.com>
2021-08-02 17:06:21 -0300

    CRUD recordatorios

---
+++ lib/src/db/db.dart
@@ -0,0 +1,50 @@
+import 'package:sqflite/sqflite.dart';
+
+import "package:path/path.dart";
+import 'package:expenses_app/src/db/recordatorios_db.dart';
+
+class DB {
+  static Future<Database> _openDB() async {
+    return openDatabase(
+      join(await getDatabasesPath(), "recordatorios.db"),
+      onCreate: (db, version) {
+        return db.execute(
+            "CREATE TABLE recordatorios (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, recordatorio TEXT, fecha TEXT)");
+      },
+      version: 1,
+    );
+  }
+
+//CRUD
+
+  static Future<dynamic> insert(Recordatorio recordatorio) async {
+    Database database = await _openDB();
+    return database.insert("recordatorios", recordatorio.toMap());
+  }
+
+  static Future<dynamic> delete(Recordatorio recordatorio) async {
+    Database database = await _openDB();
+    return database
+        .delete("recordatorios", where: 'id= ?', whereArgs: [recordatorio.id]);
+  }
+
+  static Future<dynamic> update(Recordatorio recordatorio) async {
+    Database database = await _openDB();
+    return database.update("recordatorios", recordatorio.toMap(),
+        where: 'id=?', whereArgs: [recordatorio.id]);
+  }
+
+  static Future<dynamic> getAll() async {
+    Database database = await _openDB();
+    final List<Map<String, dynamic>> recordatoriosMap =
+        await database.query("recordatorios");
+
+    return List.generate(
+      recordatoriosMap.length,
+      (index) => Recordatorio(
+          id: recordatoriosMap[index]["id"],
+          recordatorio: recordatoriosMap[index]["recordatorio"],
+          fecha: recordatoriosMap[index]["fecha"]),
+    );
+  }
+}
--- lib/src/db/recordatorios_db.dart
+++ lib/src/db/recordatorios_db.dart
@@ -1,9 +1,9 @@
-class Recordatorios {
+class Recordatorio {
   int id;
   String recordatorio;
   String fecha;
 
-  Recordatorios({this.id = 0, required this.recordatorio, required this.fecha});
+  Recordatorio({this.id = 0, required this.recordatorio, required this.fecha});
  
  Map<String, dynamic> toMap(){
    return {"id": id, "recordatorio": recordatorio, "fecha": fecha };
